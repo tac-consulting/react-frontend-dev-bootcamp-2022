76d2069b30659044d70fe84c204365deec27a40e
pallavi <pallavi.sangem@theleanapps.com>
2022-07-29 22:22:24 +0530

    value vs reference example

---
+++ ValueRef.js
@@ -0,0 +1,13 @@
+//variable
+
+var noofstaff = 14;
+let staff = noofstaff;
+console.log(`No of staff:${staff}`);
+
+// reference
+let staffdata = {
+  branch: "CSE",
+  staff: "20",
+};
+console.log("staff details..")
+console.log(`Name:${staffdata.branch} staff:${staffdata.staff}`);
