f74462ff2742702b442289b0b6b35d8734e7f3e8
Soujanya <soujanya.mamidipally@theleanapps.com>
2022-07-30 00:29:01 +0530

    Promises

---
+++ promises.js
@@ -0,0 +1,14 @@
+var promise = new Promise(function(resolve, reject) {
+	reject('Promise Rejected')
+})
+
+promise
+	.then(function(successMessage) {
+		console.log(successMessage);
+	})
+	.catch(function(errorMessage) { 
+		console.log(errorMessage);
+	});
+
+  
+  
