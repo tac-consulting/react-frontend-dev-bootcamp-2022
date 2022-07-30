6fece8548171adcb389b58698fb41ae95c1247fe
shravanisrvz <shravanikanuganti13@gmail.com>
2022-07-30 09:21:39 +0530

    indexof the item

---
+++ arrayindexof.js
@@ -0,0 +1,10 @@
+const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+
+console.log(beasts.indexOf('bison'));
+
+
+console.log(beasts.indexOf('bison', 2));
+
+console.log(beasts.indexOf('giraffe'));
+
+
