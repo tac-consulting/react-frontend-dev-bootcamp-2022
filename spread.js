a4234d2ca44620ddcdecd8c383d7084d6a145093
shravanisrvz <shravanikanuganti13@gmail.com>
2022-07-30 10:05:07 +0530

    spread operator

---
+++ spread.js
@@ -0,0 +1,14 @@
+const myVehicle = {
+    brand: 'Ford',
+    model: 'Mustang',
+    color: 'red'
+  }
+  
+  const updateMyVehicle = {
+    type: 'car',
+    year: 2021, 
+    color: 'yellow'
+  }
+  
+  const myUpdatedVehicle = {...myVehicle, ...updateMyVehicle}
+  console.log(myUpdatedVehicle);
