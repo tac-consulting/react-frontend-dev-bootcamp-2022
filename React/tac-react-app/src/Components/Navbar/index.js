e7c4547d8eee803b7959044388c8a4971486be06
Swathi Krishna <swathik@techatcore.com>
2022-08-01 01:46:01 +0530

    feature: Header navbar

--- React/tac-react-app/src/Components/Navbar/index.js
+++ React/tac-react-app/src/Components/Navbar/index.js
@@ -1,8 +1,29 @@
 import React from "react";
+import CareersPage from "../CareersPage";
+import HomePage from "../HomePage";
+import './style.css'
 
-const Navbar = () => {
-    return(
-        <h1>WELCOME TO TAC</h1>
+
+const company = {
+    name: "tech.at.core",
+    imgUrl: "https://static.ambitionbox.com/alpha/company/photos/logos/techatcore.jpg"
+}
+
+const Navbar = (props) => {
+   
+    return (
+        <>
+            <div className="navbar">
+                <div className="company-logo">
+                    <img className="logo" src={company.imgUrl} alt="Logo" />
+                    <h1> {company.name} </h1>
+                </div>
+                <div className="btn-wrapper">
+                <button> Home </button>
+                <button> Carrers</button>
+                </div>
+            </div>
+        </>
     );
 }
 
