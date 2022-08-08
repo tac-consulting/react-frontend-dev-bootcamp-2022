9334386aec2a146d9ed58d1c638edbe62d93bd09
SwathiKrishna27 <swathikrishna2627@gmail.com>
2022-08-08 15:39:44 +0530

    Initial landing page

--- React/tac-react-app/src/Components/HomePage/index.js
+++ React/tac-react-app/src/Components/HomePage/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import CareersPage from "../CareersPage";
+import Navbar from "../Navbar/index"
 import "./style.css";
+import background from "../../assets/banner.png";
 
 const HomePage = (props) => {
     return (
         <>
-            <h1>Homepage</h1>
-            <CareersPage  />
+            <div className="bg">
+                <img src={background} alt="background" />
+                <Navbar />
+                <div className="banner-content">
+                    <h1>Over decade experience with poly skills engineering specialists.</h1>
+                    <h5>Working with companies and individuals to solve hard problems with a robust, pragmatic approach and the right technology.</h5>
+                    <button onClick={""}>WORK WITH US</button>
+                </div>
+            </div>
+
         </>
     );
 }
